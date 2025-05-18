69998932dae7f57d49d08146b29f71c797eb0d9f
EniolaAdemola <it.eniolaademola@gmail.com>
2025-05-18 08:39:00 +0100

    feat: add new icon components for Love, Time, Location, and EasyBid

---
+++ assets/icon.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+
+export const LoveIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  className,
+}) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g clipPath="url(#clip0_1527_723)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12.0965 2.81002C10.8814 1.74287 9.28281 1.09496 7.53158 1.09496C3.7285 1.09496 0.645508 4.15058 0.645508 7.91987C0.645508 9.65558 1.29031 11.2488 2.36703 12.4531L12.1054 22.105L21.5555 12.7388L21.8349 12.4443C22.9116 11.2399 23.5653 9.65558 23.5653 7.91987C23.5653 4.15058 20.4823 1.09496 16.6792 1.09496C14.928 1.09496 13.3295 1.74287 12.1143 2.81004L12.1054 2.8012L12.0965 2.81002ZM12.1054 5.35984L12.1753 5.42117L13.2615 4.34461L13.3747 4.24517C14.2533 3.47362 15.4074 3.00496 16.6792 3.00496C19.4436 3.00496 21.6553 5.22149 21.6553 7.91987C21.6553 9.15681 21.195 10.2851 20.4282 11.1519L20.19 11.403L12.1054 19.4158L3.7559 11.1404C3.00616 10.2831 2.55549 9.1563 2.55549 7.91987C2.55549 5.22149 4.76724 3.00496 7.53158 3.00496C8.80337 3.00496 9.95757 3.47362 10.8362 4.24516L10.9494 4.34454L12.0356 5.42113L12.1054 5.35984Z"
+          fill="#222222"
+          stroke="#222222"
+          strokeWidth="0.000229199"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1527_723">
+          <rect
+            width="22.9198"
+            height="22.92"
+            fill="white"
+            transform="translate(0.643555 0.139938)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+export const TimeIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  className,
+}) => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M7.77194 13.88C10.9365 13.88 13.5019 11.3146 13.5019 8.14995C13.5019 4.98536 10.9365 2.41995 7.77194 2.41995C4.60738 2.41995 2.04199 4.98536 2.04199 8.14995C2.04199 11.3146 4.60738 13.88 7.77194 13.88Z"
+        stroke="#7A7A7A"
+        strokeWidth="1.16892"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.77539 4.32996V8.14996"
+        stroke="#7A7A7A"
+        strokeWidth="1.16892"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.4748 10.8494L7.77539 8.14995"
+        stroke="#7A7A7A"
+        strokeWidth="1.16892"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export const LocationIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  className,
+}) => {
+  return (
+    <svg
+      width="16"
+      height="20"
+      viewBox="0 0 16 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M7.80408 18.6949C11.1466 15.2569 14.489 12.1784 14.489 8.38093C14.489 4.58342 11.4961 1.50493 7.80408 1.50493C4.11209 1.50493 1.11914 4.58342 1.11914 8.38093C1.11914 12.1784 4.46161 15.2569 7.80408 18.6949Z"
+        stroke="#005BB5"
+        strokeWidth="1.90999"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.80452 10.1C8.8594 10.1 9.7145 9.24486 9.7145 8.18997C9.7145 7.1351 8.8594 6.27997 7.80452 6.27997C6.74963 6.27997 5.89453 7.1351 5.89453 8.18997C5.89453 9.24486 6.74963 10.1 7.80452 10.1Z"
+        stroke="#005BB5"
+        strokeWidth="1.90999"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export const EasyBidIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  className,
+}) => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <circle cx="23.9971" cy="23.9999" r="24" fill="#FF7043" />
+    </svg>
+  );
+};
